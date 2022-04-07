9017279e35a80ba5617bd44760ab2a441a23aca5
Oooooiii <hearong.200@gmail.com>
2022-04-07 13:36:09 +0700

    refactor navigation stack

--- App.tsx
+++ App.tsx
@@ -1,24 +1,8 @@
 import * as React from "react";
 import { ThemeProvider } from "@shopify/restyle";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Onboarding, Welcome } from "./src/Authentication";
 import { theme } from "./src/components";
-import { Routes } from "./src/components/Navigation";
-
-const AuthenticationStack = createStackNavigator<Routes>();
-const AuthenticationNavigator = () => {
-  return (
-    <AuthenticationStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
-      <AuthenticationStack.Screen name="Welcome" component={Welcome} />
-    </AuthenticationStack.Navigator>
-  );
-};
+import { AuthenticationNavigator } from "./src/Authentication";
 
 export default function App() {
   return (
--- src/Authentication/index.tsx
+++ src/Authentication/index.tsx
@@ -1,2 +1,22 @@
+import React from 'react'
+import { createStackNavigator } from "@react-navigation/stack";
+import { Routes } from "../components/Navigation";
+import Onboarding from "./Onboarding";
+import Welcome from "./Welcome";
+
 export { default as Onboarding } from "./Onboarding";
 export { default as Welcome } from "./Welcome";
+
+const AuthenticationStack = createStackNavigator<Routes>();
+export const AuthenticationNavigator = () => {
+  return (
+    <AuthenticationStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <AuthenticationStack.Screen name="Onboarding" component={Onboarding} />
+      <AuthenticationStack.Screen name="Welcome" component={Welcome} />
+    </AuthenticationStack.Navigator>
+  );
+};
